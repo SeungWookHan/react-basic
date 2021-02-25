9f61270e4c0c3417c516d0c23a3df7fc56db0f4f
SeungWookHan <hswook12@me.com>
2021-02-26 03:27:28 +0900

    Add: form input data change

--- myfirstapp/src/pages/about.jsx
+++ myfirstapp/src/pages/about.jsx
@@ -15,33 +15,62 @@ class Row extends React.Component {
 class About extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: '' };
+    this.state = {
+      username: '',
+      email: '',
+    };
+    this.data = [];
   }
 
-  myChangeHandler = evt => {
-    this.setState({ username: evt.target.value });
+  myChangeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
   };
 
-  doSave = () => {
-    const { username } = this.state;
-    alert(username);
+  doSave = e => {
+    e.preventDefault();
+    const { username, email } = this.state;
+    this.data.push({ name: username, mail: email });
+    // console.log(username, email);
+    // return { username, email };
+    // data.push({ name: username, email: email });
   };
 
   render() {
-    const data = [
-      { name: 'A', email: 'a' },
-      { name: 'B', email: 'b' },
-      { name: 'C', email: 'c' },
-    ]; // 서버에서 데이터를 가져왔다고 가정
-    const rows = data.map(row => <Row name={row.name} email={row.email} />);
+    // const data = [
+    //   { name: 'A', email: 'a' },
+    //   { name: 'B', email: 'b' },
+    //   { name: 'C', email: 'c' },
+    // ]; // 서버에서 데이터를 가져왔다고 가정
+    const rows = this.data.map(row => <Row name={row.name} email={row.mail} />);
 
     return (
       <div>
-        <p>Enter yout name:</p>
-        <input type="text" onChange={this.myChangeHandler} />
-        <button type="button" className="btn btn-primary" onClick={this.doSave}>
-          저장
-        </button>
+        <form onSubmit={this.doSave}>
+          <p>Enter yout name:</p>
+          <input
+            type="text"
+            placeholder="이름"
+            onChange={this.myChangeHandler}
+            name="username"
+          />
+          <p>Enter yout email:</p>
+          <input
+            type="text"
+            placeholder="이메일"
+            onChange={this.myChangeHandler}
+            name="email"
+          />
+          <br />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            // onClick={this.doSave}
+          >
+            저장
+          </button>
+        </form>
 
         <table className="table table-bordered">
           <thead>
